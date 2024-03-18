d22b4d014aa9803c877b33a3b954b839209df4c0
Ichsanul Dwi Prayitno <dwiblacklist@gmail.com>
2024-03-18 21:48:23 +0700

    Penambahan Tabel Dan Laporan

--- Project/database/migrations/2024_03_04_105827_bookr.php
+++ Project/database/migrations/2024_03_04_105827_bookr.php
@@ -11,7 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('bookr', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('date');
+            $table->timestamps();
+        });
     }
 
     /**
